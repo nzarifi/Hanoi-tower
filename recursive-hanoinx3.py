38a4a831520998124201f9d1917c6867c1c3d26a
nzarifi <49527718+nzarifi@users.noreply.github.com>
2019-04-11 18:45:24 -0400

    Update recursive-hanoinx3.py

--- recursive-hanoinx3.py
+++ recursive-hanoinx3.py
@@ -1,4 +1,6 @@
-
+"""array([[1, 0, 0],
+          [2, 0, 0],
+          [3, 0, 0]])"""
 #hanoi nx3 problem
 #numbers could jump from col1 to col3 and opposite
 import numpy as np
@@ -12,6 +14,35 @@ def moveTower(height,fromPole, toPole, withPole):
 
 def moveDisk(fp,tp):
  print("moving disk from",fp,"to",tp)
- 
+##output would be: 
+moveTower(3,1,2,3)
+i: 1 2
+i: 2 1
+i: 3 0
+j: 1
+moving disk from 1 to 2
+k: 1 0
+j: 2
+moving disk from 1 to 3
+k: 2 1
+i: 4 0
+j: 3
+moving disk from 2 to 3
+k: 3 0
+j: 4
+moving disk from 1 to 2
+k: 4 2
+i: 5 1
+i: 6 0
+j: 5
+moving disk from 3 to 1
+k: 5 0
+j: 6
+moving disk from 3 to 2
+k: 6 1
+i: 7 0
+j: 7
+moving disk from 1 to 2
+k: 7 0
 
 
